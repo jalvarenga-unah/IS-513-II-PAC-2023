92a8f5d80cc2b1d3b5f0988d5eb75f0312c03c2a
jalvarenga-unah <jealvarengar@unah.edu.hn>
2023-05-30 17:58:10 -0600

    introduccion a dart, varibales

---
+++ intro-dart/01-variables.dart
@@ -0,0 +1,22 @@
+void main() {
+  //Declarción
+
+  int numero = 10;
+  double doble = 3.09;
+  String nombre = 'Juan';
+  //var apellido;
+  final String apellido = 'Alvarenga';
+  bool esMayor = true;
+  //List comidas = [];
+
+  print(numero);
+  print(doble);
+
+  nombre = 'Enrique';
+
+  //apellido = 100;
+  // apellido = 'Alvarenga';
+
+  print(nombre);
+  print(apellido);
+}
