d39750acbc722283c2a7be5b10810899f3115411
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2023-08-02 17:49:45 -0600

    integracion del PageView, drawer y opciones en el appbar

--- proyectos-flutter/app_menus/lib/main.dart
+++ proyectos-flutter/app_menus/lib/main.dart
@@ -10,31 +10,75 @@ class MyApp extends StatelessWidget {
 
   // int currentIndex = 0;
   final homeController = Get.put(HomeController());
+  final pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Instagram',
       theme: ThemeData(useMaterial3: true),
       home: Scaffold(
-          // appBar: AppBar(
-          //   title: const Text('App de Menú'),
-          // ),
-          body: const SafeArea(
-            child: Column(
+          appBar: AppBar(
+            title: const Text('Instagram'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.send),
+              ),
+            ],
+          ),
+          drawer: Drawer(
+            child: ListView(
               children: [
-                Center(
-                  child: Text('Hola Mundo'),
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Inicio'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Configuración'),
+                  onTap: () {},
                 ),
               ],
             ),
           ),
+          body: PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              homeController.currentIndex = index;
+            },
+            // scrollDirection: Axis.vertical,
+            // physics:
+            //     const NeverScrollableScrollPhysics(), // evita el swipe manual
+            children: const [
+              HomePage(),
+              ConfiguracionPage(),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
           bottomNavigationBar: Obx(
             () => BottomNavigationBar(
               currentIndex: homeController.currentIndex,
               onTap: (index) {
                 homeController.currentIndex = index;
+                pageController.animateToPage(index,
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.easeInOut);
               },
               items: const [
                 BottomNavigationBarItem(
@@ -48,6 +92,44 @@ class MyApp extends StatelessWidget {
               ],
             ),
           )),
+    );
+  }
+}
+
+class ConfiguracionPage extends StatelessWidget {
+  const ConfiguracionPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue[100],
+      child: const Center(
+        child: Text(
+          'Página de configuración',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple[100],
+      child: const Center(
+        child: Text(
+          'Pagina de inicio',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
     );
   }
 }
